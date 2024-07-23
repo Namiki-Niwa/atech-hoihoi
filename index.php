b2be5ab8270d7cc1bd12dd3cb8c7ef231db5cfee
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-07-24 02:13:30 +0800

    edit

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <!-- 検索にヒットしないようにするための記述 -->
+  <meta name="robots" content="noindex">
+  <title>メンズエステに特化した攻めのDSP｜客人HoiHoi｜待つだけのポータルから攻めのバナー広告へ ｜集客・求人どちらにもご利用可能</title>
+  <meta name="description" content="メンズエステに特化した攻めのバナー広告なら客人HoiHoi /集客・求人どちらにも利用可能で、バニラやエス魂などへも連携可能して掲載ランクのアップにも利用可能です。／ひと月16,500円より">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/destyle.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 </head>
 
 <body>
 
-  <h1>Dexter</h1>
+  <h1 class="title">Dexter</h1>
 
 </body>
 
