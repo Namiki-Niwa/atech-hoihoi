8846d0d9694185448ade650aa8832250faa8a8f8
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-07-30 00:23:43 +0800

    segment

--- front-page.php
+++ front-page.php
@@ -1,84 +1,136 @@
-<!DOCTYPE html>
-<html lang="ja">
+  <?php get_header(); ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- 検索にヒットしないようにするための記述 -->
-  <meta name="robots" content="noindex">
-  <title>メンズエステに特化した攻めのDSP｜客人HoiHoi｜待つだけのポータルから攻めのバナー広告へ ｜集客・求人どちらにもご利用可能</title>
-  <meta name="description" content="メンズエステに特化した攻めのバナー広告なら客人HoiHoi /集客・求人どちらにも利用可能で、バニラやエス魂などへも連携可能して掲載ランクのアップにも利用可能です。／ひと月16,500円より">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/destyle.css">
-  <!-- <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css"> -->
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-</head>
-
-<body>
-  <div class="wrapper">
-
-    <header class="header">
-      <div class="header__inner inner">
-        <div class="header__logo"><img src="<?php echo get_template_directory_uri(); ?>/images/header-logo.png" alt=""></div>
-        <nav class="header__nav">
-          <ol class="header__nav-list">
-            <li class="header__nav-item"><a href="#">Maroudo HoiHoiの特徴</a></li>
-            <li class="header__nav-item"><a href="#">⾼品質なCookieデータを活⽤</a></li>
-            <li class="header__nav-item"><a href="#">配信価格</a></li>
-            <li class="header__nav-item"><a href="#" class="btn btn-contact">お問い合わせ</a></li>
-          </ol>
-        </nav>
+  <main class="main">
+    <div class="fv">
+      <div class="fv__decoration">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/deco1.png" alt="" class="deco1">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/deco2.png" alt="" class="deco2">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/deco3.png" alt="" class="deco3">
       </div>
-    </header>
-
-    <main class="main">
-      <div class="fv">
-        <div class="fv__inner inner">
-          <div class="fv__decoration">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/deco1.png" alt="" class="deco1">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/deco2.png" alt="" class="deco2">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/deco3.png" alt="" class="deco3">
-          </div>
-          <div class="fv__container">
-            <p class="fv__text">待つだけの広告はもうおさらばして<br>これからは攻めの広告に！</p>
-            <h1 class="fv__title"><span>メンズエステ</span>に<br><span>特化</span>した<span>広告</span>配信</h1>
-            <div class="fv__features">
-              <div class="fv__features-item">
-                <p class="fv__features-text">ポータルサイト<br>以外からの新しい<br>ユーザー動線を<br>確保</p>
-              </div>
-              <div class="fv__features-item">
-                <p class="fv__features-text">メンエスユーザーに<br>限定した誘導が<br>可能</p>
-              </div>
-              <div class="fv__features-item">
-                <p class="fv__features-text">集客/求人<br>どちらにも<br>ターゲットが<br>可能</p>
-              </div>
-              <div class="fv__features-item">
-                <p class="fv__features-text">日常生活での<br>サイト閲覧中に<br>リーチができ<br>認知が早い</p>
-              </div>
+      <div class="fv__inner inner">
+        <div class="fv__container">
+          <p class="fv__text">待つだけの広告はもうおさらばして<br>これからは攻めの広告に！</p>
+          <h1 class="fv__title"><span>メンズエステ</span>に<br><span>特化</span>した<span>広告</span>配信</h1>
+          <div class="fv__features">
+            <div class="fv__features-item">
+              <p class="fv__features-text">ポータルサイト<br>以外からの新しい<br>ユーザー動線を<br>確保</p>
+            </div>
+            <div class="fv__features-item">
+              <p class="fv__features-text">メンエスユーザーに<br>限定した誘導が<br>可能</p>
+            </div>
+            <div class="fv__features-item">
+              <p class="fv__features-text">集客/求人<br>どちらにも<br>ターゲットが<br>可能</p>
+            </div>
+            <div class="fv__features-item">
+              <p class="fv__features-text">日常生活での<br>サイト閲覧中に<br>リーチができ<br>認知が早い</p>
             </div>
           </div>
         </div>
       </div>
+    </div>
 
-      <section class="feature">
-        <div class="title__wrapper">
+    <section class="section__feature">
+      <div class="section__feature-inner">
+        <div class="section__title-wrapper">
           <h2 class="section__title">Maroudo HoiHoiの<br>特徴</h2>
-          <p class="title__text">
+          <p class="section__title-text">
             800億imp/⽉・1億UUを保有する、SSPを直接買い付けることにより<br>
             ⾼品質なオーディエンスに低価格での配信を実現。
           </p>
         </div>
         <div class="feature__content">
-          <div class="feature__item">
-
+          <div class="feature__item feature-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/menu105.png" alt="" class="feature-item__img">
+            <p class="feature-item__number">POINT<span>1</span></p>
+            <h3 class="feature-item__title">低価格で集客が可能</h3>
+            <div class="feature-item__text-wrapper">
+              <p class="feature-item__text">SSPにダイレクトに入札することで中間マージンを圧縮</p>
+              <p class="feature-item__text">独自アルゴリズムにより配信を最適化</p>
+            </div>
+          </div>
+          <div class="feature__item feature-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/menu106.png" alt="" class="feature-item__img">
+            <p class="feature-item__number">POINT<span>2</span></p>
+            <h3 class="feature-item__title">高品質なイベントリーを確保</h3>
+            <div class="feature-item__text-wrapper">
+              <p class="feature-item__text">良質な在庫を確保しているSSPへ接続</p>
+              <p class="feature-item__text">他社のDSPよりも優先的な買い付けが可能</p>
+            </div>
+          </div>
+          <div class="feature__item feature-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/menu107.png" alt="" class="feature-item__img">
+            <p class="feature-item__number">POINT<span>3</span></p>
+            <h3 class="feature-item__title">高品質なCookieデータを活用</h3>
+            <div class="feature-item__text-wrapper">
+              <p class="feature-item__text">業界に関するサイト閲覧者のオーディエンスデータを活用</p>
+              <p class="feature-item__text">属性のセグメントにより、集客と求人を同時にターゲティング</p>
+            </div>
           </div>
         </div>
+      </div>
+      <div class="feature3">
+        <div class="feature3__inner">
+          <div class="section__title-wrapper">
+            <h2 class="section__title">高品質な<br>Cookieデータを活用</h2>
+            <p class="feature3__title-text section__title-text">
+              メンズエステの関連サイトにアクセスしているユーザーに対しターゲティングを⾏います。<br>
+              媒体のAI機能で、ユーザーの類似ユーザーをセグメントし配信を⾏うことで⾮認知のユーザーにもアプローチをして、<br>
+              ホームページに誘導し、⾒込み客を獲得します。
+            </p>
+          </div>
+          <div class="feature3__content">
+            <div class="feature3__user-wrapper">
+              <div class="feature3__user">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/menu108.png" alt="" class="feature3__img">
+                <p class="feature3__user-text">アクセスエリアをセグメントして貴店<br>のHPのトップページへ誘導</p>
+              </div>
+              <div class="feature3__line"></div>
+              <div class="feature3__user">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/menu109.png" alt="" class="feature3__img">
+                <p class="feature3__user-text">アクセスエリアをセグメントして貴店<br>のHPのトップページへ誘導</p>
+              </div>
+            </div>
+            <div class="triangle"></div>
+            <p class="feature3__text">メンズエステを認知しているユーザーに対して<br>集客と求人が同時に可能</p>
+          </div>
+        </div>
+      </div>
+    </section>
 
-      </section>
-    </main>
+    <section class="section__segment">
+      <div class="section__segment-inner inner">
+        <div class="section__title-wrapper">
+          <h2 class="section__title">セグメント</h2>
+        </div>
+        <dl class="segment__description">
+          <dt>性別</dt>
+          <dd>男性/女性</dd>
+        </dl>
+        <dl class="segment__description">
+          <dt>年代別</dt>
+          <dd>20代/30代/40代/50代/60代以上</dd>
+        </dl>
+        <dl class="segment__description">
+          <dt>エリア</dt>
+          <dd>都道府県単位</dd>
+        </dl>
+        <dl class="segment__description">
+          <dt>曜日</dt>
+          <dd>月曜日〜日曜日の指定が可能</dd>
+        </dl>
+        <dl class="segment__description">
+          <dt>時間</dt>
+          <dd>00:00〜23:59まで1分単位で設定可能</dd>
+        </dl>
+        <dl class="segment__description">
+          <dt>OC/キャリア</dt>
+          <dd>PC/iOS/Android</dd>
+        </dl>
+        <p class="segment__text">※デフォルトのセグメントは性別のみとなります</p>
+        <p class="segment__text">※セグメントを追加・変更する場合は、配信数の保証対象外になる場合があります</p>
+      </div>
+    </section>
+  </main>
+  </body>
 
-
-
-  </div>
-</body>
-
-</html>
+  </html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- 検索にヒットしないようにするための記述 -->
+  <meta name="robots" content="noindex">
+  <title>メンズエステに特化した攻めのDSP｜客人HoiHoi｜待つだけのポータルから攻めのバナー広告へ ｜集客・求人どちらにもご利用可能</title>
+  <meta name="description" content="メンズエステに特化した攻めのバナー広告なら客人HoiHoi /集客・求人どちらにも利用可能で、バニラやエス魂などへも連携可能して掲載ランクのアップにも利用可能です。／ひと月16,500円より">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/destyle.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner inner">
+      <div class="header__logo"><img src="<?php echo get_template_directory_uri(); ?>/images/header-logo.png" alt=""></div>
+      <nav class="header__nav">
+        <ol class="header__nav-list">
+          <li class="header__nav-item"><a href="#">Maroudo HoiHoiの特徴</a></li>
+          <li class="header__nav-item"><a href="#">⾼品質なCookieデータを活⽤</a></li>
+          <li class="header__nav-item"><a href="#">配信価格</a></li>
+          <li class="header__nav-item"><a href="#" class="btn btn-contact">お問い合わせ</a></li>
+        </ol>
+      </nav>
+    </div>
+  </header>
