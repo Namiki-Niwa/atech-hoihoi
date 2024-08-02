2c6aa338d4e1892c3950c74f52eb1e2f1eb64a5d
Namiki-Niwa <109219684+Namiki-Niwa@users.noreply.github.com>
2024-08-02 10:42:43 +0800

    modal-regulation_0802-1

--- functions.php
+++ functions.php
@@ -6,9 +6,9 @@ function theme_enqueue_assets()
   wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
 
   // デフォルトのjQueryの読み込み
-  wp_enqueue_script('jQuery');
+  wp_enqueue_script('jquery');
 
   // カスタムスクリプトの読み込み
-  wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/index/js', array('jquery'), null, true);
+  wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/index.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_assets');
--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
-jQuery(document).ready(function($){
-  $('#al').on("click", function(){
-    alert("正常に発動しました");
-  });
+// jQuery(document).ready(function($){
+//   $('#btn').on("click", function(){
+//     alert("正常に発動しました");
+//   });
+// });
+
+jQuery('#al').on("click", function(){
+  alert("suceed!");
 });
+
+jQuery('#modal-open').on("click", function (){
+  console.log("test");
+  jQuery('#modal-container').addClass('is-active');
+  return false;
+});
+
+
+
+// $(function(){
+// 	// 変数に要素を入れる
+// 	var open = $('.modal__open'),
+// 		close = $('.modal__close'),
+// 		container = $('.modal__container');
+
+// 	//開くボタンをクリックしたらモーダルを表示する
+// 	open.on('click',function(){	
+// 		container.addClass('active');
+// 		return false;
+// 	});
+
+// 	//閉じるボタンをクリックしたらモーダルを閉じる
+// 	close.on('click',function(){	
+// 		container.removeClass('active');
+// 	});
+
+// 	//モーダルの外側をクリックしたらモーダルを閉じる
+// 	$(document).on('click',function(e) {
+// 		if(!$(e.target).closest('.modal__body').length) {
+// 			container.removeClass('active');
+// 		}
+// 	});
+// });
